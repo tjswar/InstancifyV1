a3dae5898038ea530e1aa4a62e507fe461c915df
tjswar <tejaswar8484@gmail.com>
2025-02-18 20:30:12 -0800

    Initial commit: Core Instancify files

--- functions/src/config.ts
+++
@@ -1,9 +0,0 @@
-import * as admin from "firebase-admin";
-
-// Initialize Firebase Admin if not already initialized
-if (!admin.apps.length) {
-  admin.initializeApp();
-}
-
-// Get Firestore instance
-export const db = admin.firestore();
